84a63fcd561e518856d9ec53ff36096a0b9e6da8
rbgoncalves <r.goncalves@xgeeks.io>
2022-05-15 14:50:19 +0100

    feat: fix getProposals script

--- Project/scripts/Ballot/getProposals.ts
+++ Project/scripts/Ballot/getProposals.ts
@@ -1,22 +1,35 @@
 import { Contract, ethers } from "ethers";
 import "dotenv/config";
 import * as ballotJson from "../../artifacts/contracts/Ballot.sol/Ballot.json";
-// eslint-disable-next-line node/no-missing-import
 import { Ballot } from "../../typechain";
-import { connectToBlockchain } from "./utils"
+import { connectToBlockchain } from "./utils";
 
 async function main() {
-    const signer = await connectToBlockchain();
-    const ballotAddress = "0x0ca02ed57ed09f2ac3bf2809dd1ea91410ac00c2";
-    const ballotContract: Ballot = new Contract(
-        ballotAddress,
-        ballotJson.abi,
-        signer
-    ) as Ballot;
-    const proposalArray = ballotContract.proposals;
-    for (let index = 0; index < proposalArray.length; index++) {
-        const proposal = await proposalArray(index);
-        const proposalName = ethers.utils.parseBytes32String(proposal.name);
-        console.log(`proposal ${index} is called ${proposalName}`);
+  const signer = await connectToBlockchain();
+  const ballotAddress = "0x0ca02ed57ed09f2ac3bf2809dd1ea91410ac00c2";
+  const ballotContract: Ballot = new Contract(
+    ballotAddress,
+    ballotJson.abi,
+    signer
+  ) as Ballot;
+
+  let index = 0;
+
+  while (index !== -1) {
+    try {
+      const proposal = await ballotContract.proposals(index);
+      const proposalName = ethers.utils.parseBytes32String(proposal.name);
+
+      console.log(`Proposal ${index} has name ${proposalName}`);
+      index++;
+    } catch (e) {
+      index = -1;
+      break;
     }
+  }
 }
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
