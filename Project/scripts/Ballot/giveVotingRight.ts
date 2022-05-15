0f995927f76a2bd0c567950875489336833864c5
rbgoncalves <r.goncalves@xgeeks.io>
2022-05-15 21:12:54 +0100

    refactor: cleanup

--- Project/scripts/Ballot/giveVotingRight.ts
+++ Project/scripts/Ballot/giveVotingRight.ts
@@ -1,12 +1,8 @@
-import { Contract, ethers } from "ethers";
+import { Contract } from "ethers";
 import "dotenv/config";
 import * as ballotJson from "../../artifacts/contracts/Ballot.sol/Ballot.json";
-// eslint-disable-next-line node/no-missing-import
 import { Ballot } from "../../typechain";
-import { connectToBlockchain } from "./utils"
-
-// This key is already public on Herong's Tutorial Examples - v1.03, by Dr. Herong Yang
-// Do never expose your keys like this
+import { connectToBlockchain } from "./utils";
 
 async function main() {
   const signer = await connectToBlockchain();
@@ -17,14 +13,19 @@ async function main() {
   console.log(
     `Attaching ballot contract interface to address ${ballotAddress}`
   );
+
   const ballotContract: Ballot = new Contract(
     ballotAddress,
     ballotJson.abi,
     signer
   ) as Ballot;
+
   const chairpersonAddress = await ballotContract.chairperson();
-  if (chairpersonAddress !== signer.address)
+
+  if (chairpersonAddress !== signer.address) {
     throw new Error("Caller is not the chairperson for this contract");
+  }
+
   console.log(`Giving right to vote to ${voterAddress}`);
   const tx = await ballotContract.giveRightToVote(voterAddress);
   console.log("Awaiting confirmations");
