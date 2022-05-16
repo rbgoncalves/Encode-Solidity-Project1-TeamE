eb6c0519d577dbd60f0b50f31fe8e47c35c4a5e7
rbgoncalves <r.goncalves@xgeeks.io>
2022-05-16 10:56:52 +0100

    refactor: make getResults consistent

--- Project/scripts/Ballot/getResults.ts
+++ Project/scripts/Ballot/getResults.ts
@@ -1,26 +1,19 @@
 // query the results
-import { Contract, ethers } from "ethers";
+import { ethers } from "ethers";
 import "dotenv/config";
-import * as ballotJson from "../../artifacts/contracts/Ballot.sol/Ballot.json";
-// eslint-disable-next-line node/no-missing-import
-import { Ballot } from "../../typechain";
-import { connectToBlockchain } from "./utils"
+import { connectToBlockchain, connectToContract, getArg } from "./utils";
 
 async function main() {
-    const signer = await connectToBlockchain();
-    const ballotAddress = "0x0ca02ed57ed09f2ac3bf2809dd1ea91410ac00c2";
-    const ballotContract: Ballot = new Contract(
-        ballotAddress,
-        ballotJson.abi,
-        signer
-    ) as Ballot;
+  const signer = await connectToBlockchain();
 
+  const ballotAddress = getArg(2, "Ballot address missing");
+  const ballotContract = connectToContract(ballotAddress, signer);
 
-    const winner = await ballotContract.winnerName();
-    const winnerString = ethers.utils.parseBytes32String(winner);
-    console.log(`The winner is proposal ${winnerString}`);
+  const winner = await ballotContract.winnerName();
+  const winnerString = ethers.utils.parseBytes32String(winner);
+  console.log(`The winner is proposal ${winnerString}`);
 }
 main().catch((error) => {
-    console.error(error);
-    process.exit(1);
+  console.error(error);
+  process.exit(1);
 });
